b40871745d34564e631bfc5c24ac071635b94d0a
Simon Engwerda <simon@engwerda.net>
2017-04-21 17:34:06 +0700

    Wrap add children to context menu in a function.

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,8 +1,7 @@
 // Load the options page on install.
-chrome.runtime.onInstalled.addListener(function(){
+chrome.runtime.onInstalled.addListener(function() {
     chrome.runtime.openOptionsPage();
 });
-
 
 // Function to add children to a parent Context Menu.
 function addChildToContextMenu(title, bang, parentMenu = parentContextMenu) {
@@ -28,72 +27,75 @@ const parentContextMenu = chrome.contextMenus.create({
 });
 
 // Add children when set in options.
-chrome.storage.local.get(null, function(items) {
+function addChildrenToContextMenu() {
+    chrome.storage.local.get(null, function(items) {
 
-    if (items.duckDuckGoLink === true) {
-        addChildToContextMenu('DuckDuckGo', '');
-    }
-    if (items.googleLink === true) {
-        addChildToContextMenu('Google', '!g');
-    }
-    if (items.youtubeLink === true) {
-        addChildToContextMenu('Youtube', '!yt');
-    }
-    if (items.googleImagesLink === true) {
-        addChildToContextMenu('Google Images', '!gi');
-    }
-    if (items.googleMapsLink === true) {
-        addChildToContextMenu('Google Maps', '!gm');
-    }
-    if (items.googleTranslateLink === true) {
-        addChildToContextMenu('Google Translate', '!tr');
-    }
-    if (items.dictionaryLink === true) {
-        addChildToContextMenu('Dictionary.com', '!di');
-    }
-    if (items.googleNewsLink === true) {
-        addChildToContextMenu('Google News', '!gn');
-    }
-    if (items.hackerNewsLink === true) {
-        addChildToContextMenu('Hacker News', '!hn');
-    }
-    if (items.facebookLink === true) {
-        addChildToContextMenu('Facebook', '!fb');
-    }
-    if (items.twitterLink === true) {
-        addChildToContextMenu('Twitter', '!tw');
-    }
-    if (items.wikipediaLink === true) {
-        addChildToContextMenu('Wikipedia', '!wi');
-    }
-    if (items.imdLink === true) {
-        addChildToContextMenu('IMDb', '!imd');
-    }
-    if (items.bingLink === true) {
-        addChildToContextMenu('Bing', '!b');
-    }
-    if (items.githubLink === true) {
-        addChildToContextMenu('GitHub', '!git');
-    }
-    if (items.stackOverflowLink === true) {
-        addChildToContextMenu('StackOverflow', '!so');
-    }
-    if (items.redditLink === true) {
-        addChildToContextMenu('Reddit', '!r');
-    }
-    if (items.amazonLink === true) {
-        addChildToContextMenu('Amazon', '!a');
-    }
-    if (items.piratebayLink === true) {
-        addChildToContextMenu('Piratebay', '!piratebay');
-    }
-    if (items.avaxhomeLink === true) {
-        addChildToContextMenu('AvaxHome', '!avax');
-    }
-    if (items.rarbgLink === true) {
-        addChildToContextMenu('RaRBG', '!rarbg');
-    }
-});
+        if (items.duckDuckGoLink === true) {
+            addChildToContextMenu('DuckDuckGo', '');
+        }
+        if (items.googleLink === true) {
+            addChildToContextMenu('Google', '!g');
+        }
+        if (items.youtubeLink === true) {
+            addChildToContextMenu('Youtube', '!yt');
+        }
+        if (items.googleImagesLink === true) {
+            addChildToContextMenu('Google Images', '!gi');
+        }
+        if (items.googleMapsLink === true) {
+            addChildToContextMenu('Google Maps', '!gm');
+        }
+        if (items.googleTranslateLink === true) {
+            addChildToContextMenu('Google Translate', '!tr');
+        }
+        if (items.dictionaryLink === true) {
+            addChildToContextMenu('Dictionary.com', '!di');
+        }
+        if (items.googleNewsLink === true) {
+            addChildToContextMenu('Google News', '!gn');
+        }
+        if (items.hackerNewsLink === true) {
+            addChildToContextMenu('Hacker News', '!hn');
+        }
+        if (items.facebookLink === true) {
+            addChildToContextMenu('Facebook', '!fb');
+        }
+        if (items.twitterLink === true) {
+            addChildToContextMenu('Twitter', '!tw');
+        }
+        if (items.wikipediaLink === true) {
+            addChildToContextMenu('Wikipedia', '!wi');
+        }
+        if (items.imdLink === true) {
+            addChildToContextMenu('IMDb', '!imd');
+        }
+        if (items.bingLink === true) {
+            addChildToContextMenu('Bing', '!b');
+        }
+        if (items.githubLink === true) {
+            addChildToContextMenu('GitHub', '!git');
+        }
+        if (items.stackOverflowLink === true) {
+            addChildToContextMenu('StackOverflow', '!so');
+        }
+        if (items.redditLink === true) {
+            addChildToContextMenu('Reddit', '!r');
+        }
+        if (items.amazonLink === true) {
+            addChildToContextMenu('Amazon', '!a');
+        }
+        if (items.piratebayLink === true) {
+            addChildToContextMenu('Piratebay', '!piratebay');
+        }
+        if (items.avaxhomeLink === true) {
+            addChildToContextMenu('AvaxHome', '!avax');
+        }
+        if (items.rarbgLink === true) {
+            addChildToContextMenu('RaRBG', '!rarbg');
+        }
+    });
+}
+addChildrenToContextMenu();
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
     chrome.tabs.query({
